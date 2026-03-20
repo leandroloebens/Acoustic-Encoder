451c30b325d8df4123419484ea5de6efad62e589
leandro loebens <leandrohenriqueloebens@gmail.com>
2026-03-19 21:32:48 -0300

    wip: bootstrap NotesMapper and adjust notesMap.properties format

---
+++ src/main/java/com/acoustic/encoder/parser/NotesMapper.java
@@ -0,0 +1,17 @@
+package com.acoustic.encoder.parser;
+
+import java.util.Map;
+
+public class NotesMapper {
+
+    private final String NOTES_MAP_FILE = "notesMap.properties";
+
+    private Map<Character, String> notesMap;
+
+    private Map<Character, String> loadMap(String fileName) {
+
+
+
+        return null;
+    }
+}
